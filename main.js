f693ca37e1562050a26c35dd997ab48dd200587e
Milan Nakani <milannakani@gmail.com>
2022-08-29 21:19:45 +0530

    done with last changes

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 let color = "black";
+let click = true;
 
 function populateGrid (size) {
 let board = document.querySelector(".grid");
 let squares = board.querySelectorAll("div")
-squares.forEach(div => div.remove());
+//squares.forEach(div => div.remove());
 board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
@@ -11,25 +12,31 @@ let amount = size*size;
 for (let i = 0; i < amount; i++) {
     let square = document.createElement("div");
     square.addEventListener("mouseover", colorSquare); 
-  //  square.style.backgroundColor = "white";
+    square.style.backgroundColor = "white";
     board.insertAdjacentElement("beforeend",square);
 }
 }
+populateGrid(16);
 
 function changeSize(input) {
-    if (input >=2 || input<=100) {
-    populateGrid(input); 
+    if (input >=2 && input<=100) {
+        document.querySelector(".error").style.display = "none";
+        populateGrid(input); 
     }
     else {
-        console.log("many squares");
+        populateGrid(0);
+        document.querySelector(".error").style.display = "flex";
     }
 }
+
 function colorSquare () {
+    if (click) {
     if (color === "random") {
         this.style.backgroundColor = `hsl(${Math.random()*360},100%,50%)`;
     }
     else {
         this.style.backgroundColor = color;
+    }
     }
 }
 
@@ -41,3 +48,15 @@ function resetBoard () {
     let squares = board.querySelectorAll("div")
     squares.forEach(div => div.style.backgroundColor = "white");
 }
+
+document.querySelector(".content").addEventListener("click", (e) => {
+    if (e.target.tagName != "BUTTON") {
+    click = !click;
+    if (click) {
+        document.querySelector(".mode").textContent = "Coloring";
+    }
+    else {
+        document.querySelector(".mode").textContent = "Not-Coloring";
+    }
+    }
+});
